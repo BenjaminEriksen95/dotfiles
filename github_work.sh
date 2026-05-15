9ddbfe38f8d279429539c89cb402484bf397a154
Benjamin Eriksen <Benja.e95@gmail.com>
2026-05-15 15:13:38 +0200

    ssh

--- github_work.sh
+++ github_work.sh
@@ -3,7 +3,7 @@
 # Exit on error
 set -e
 
-# Function to set up SSH config and keys
+# Function to set up SSH config and keys for work GitHub account (be_festina)
 setup_ssh() {
     # Create .ssh directory if it doesn't exist
     mkdir -p ~/.ssh
@@ -12,21 +12,21 @@ setup_ssh() {
     stow ssh
 
     # Generate SSH key
-    ssh-keygen -t ed25519 -C "benja.e95@gmail.com" -f ~/.ssh/id_ed25519 -N ""
+    ssh-keygen -t ed25519 -C "be@festinafinance.com" -f ~/.ssh/id_work -N ""
 
     # Start the SSH agent
     eval "$(ssh-agent -s)"
 
     # Add SSH private key to the agent
-    ssh-add ~/.ssh/id_ed25519
+    ssh-add ~/.ssh/id_work
 
     # Display the SSH key and instructions to add it to GitHub
-    echo "Your SSH key has been generated. Copy the following public key and add it to your GitHub account:"
-    cat ~/.ssh/id_ed25519.pub
+    echo "Your SSH key has been generated. Copy the following public key and add it to your GitHub account (be_festina):"
+    cat ~/.ssh/id_work.pub
 
     echo "Visit https://github.com/settings/keys to add the key."
 
-    echo "Test using 'ssh -T git@github.com' - do not use Work wifi for this!"
+    echo "Test using 'ssh -T git@github.com-work' after adding the key."
 
     echo "SSH setup complete."
 }
